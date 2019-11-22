437ee7f9e3cd39dee35de71942fafaa11ea21bb2
Noman Ali Abbasi <noman.ali.abbasy@gmail.com>
2019-11-22 11:47:27 +0500

    Create PrintDemo.go
    Added print demo

---
+++ src/github.com/sanitary/util/PrintDemo.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alexbrainman/printer"
+	"strings"
+	"os"
+)
+
+
+func main(){
+	printers, _ := printer.ReadNames()
+	
+	defaultPrinter, _ := printer.Default()	
+	
+	for i, printerName := range printers {
+		s := " "
+		if printerName == defaultPrinter {
+			s = "*"
+		}
+		fmt.Printf(" %s %d. %s\n", s, i, printerName)
+	}
+
+	p, _ := printer.Open(defaultPrinter)	
+	defer p.Close()
+
+	_ = p.StartRawDocument("This a \ntest \ndocument")	
+	defer p.EndDocument()
+
+	_ = p.StartPage()	
+	lines := strings.Split("This a \ntest \ndocument", "\n")
+	for _, line := range lines {
+		fmt.Fprintf(p, "%s\r\n", line)
+	}
+
+	os.Exit(0)
+}
